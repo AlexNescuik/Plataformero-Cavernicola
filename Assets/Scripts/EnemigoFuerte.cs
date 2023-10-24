d705fb94df5278f13f3f165e2df25f4e57d4ce13
Alex Nescuik <alexiamomos@gmail.com>
2023-10-23 22:40:45 -0600

    Tarea update 24/10
    dasdas

--- Assets/Scripts/EnemigoFuerte.cs
+++ Assets/Scripts/EnemigoFuerte.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemigoPequeno : MonoBehaviour
+public class EnemigoFuerte : MonoBehaviour
 {
     public Transform Cavernicola;
     public Transform Hongo;
@@ -10,7 +10,8 @@ public class EnemigoPequeno : MonoBehaviour
     public float velocidadCaminar = 5;
     private bool cerca = false;
     private bool lejos;
-
+    public float distanciaAtaque = 3;
+    public float rangoAgro = 3;
 
     private Rigidbody2D miCuerpo;
     private Animator miAnimador;
@@ -19,39 +20,11 @@ public class EnemigoPequeno : MonoBehaviour
 
     public GameObject SangrePrefab;
     public GameObject corazonRotoPrefab;
-    public float rangoAgro = 3;
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        GameObject enemigoP = collision.gameObject;
-        if (enemigoP.tag == "Player")
-        {
-
-            print(name + " Enemigo cerca " + enemigoP);
-            cerca = true;
-
-
-        }
-
-
-
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        GameObject enemigoP = collision.gameObject;
-        if (enemigoP.tag == "Player")
-        {
-            print(name + " Enemigo lejos " + enemigoP);
-            cerca = false;
-        }
-    }
 
     void Start()
     {
         miCuerpo = GetComponent<Rigidbody2D>();
         miAnimador = GetComponent<Animator>();
-
         heroeJugador = GameObject.FindGameObjectWithTag("Player");
     }
 
@@ -59,20 +32,22 @@ public class EnemigoPequeno : MonoBehaviour
 
     {
         Vector3 miPos = this.transform.position;
-
         Vector3 posHeroe = heroeJugador.transform.position;
-
         float distanciaHeroe = (miPos - posHeroe).magnitude;
-
 
         if (distanciaHeroe < rangoAgro)
         {
             cerca = true;
         }
-
         else
         {
             cerca = false;
+        }
+
+        if (distanciaHeroe < distanciaAtaque)
+        {
+            miAnimador.SetTrigger("GOLPEAR");
+            miAnimador.SetBool("caminando", false);
         }
 
 
@@ -95,26 +70,19 @@ public class EnemigoPequeno : MonoBehaviour
                    new Vector3(velocidadCaminar, 0, 0);
                 miAnimador.SetBool("caminando", true);
             }
-
         }
-
         else
         {
             miAnimador.SetBool("caminando", false);
         }
-
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     { //Este metodo se ejecuta cuando este objeto detecta una colision
-
-
         GameObject otroObjeto = collision.gameObject;
         if (otroObjeto.tag == "Player")
         {
-
             print(name + " detecte colision con " + otroObjeto);
-
 
             //Con estas instrucciones obtengo el componente Personaje del Player 
             Personaje elPerso = otroObjeto.GetComponent<Personaje>();
@@ -132,11 +100,6 @@ public class EnemigoPequeno : MonoBehaviour
                 GameObject efectoCorazon = Instantiate(corazonRotoPrefab);
                 efectoCorazon.transform.position = elPerso.transform.position;
             }
-
-
         }
     }
-
-
-
 }
