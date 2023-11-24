20110c162e8b04020450d733388b2732569596a1
Alex Nescuik <alexiamomos@gmail.com>
2023-11-23 20:10:51 -0600

    update nosé
    actualicé cosas que me daban error lol

---
+++ Assets/Scripts/Meta.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meta : MonoBehaviour
+{
+    private Animator miAnimador;
+    private EfectosSonoros misSonidos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        misSonidos = GetComponent<EfectosSonoros>();
+        miAnimador = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameObject otro = collision.gameObject;
+            Personaje elPerso = otro.GetComponent<Personaje>();
+            print(name + " llegó a la meta!! " + otro);
+            miAnimador.SetTrigger("Meta");
+            misSonidos.reproducir("Meta");
+        }
+    }
+}
